40f096987c952429b1d4b9a3fea00a9813da0efd
AbrahamCastilloV <abcastillo.yo@gmail.com>
2022-12-22 18:11:31 -0700

    primer commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+export default function Error({children}) {
+  return (
+    <div className="bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+        {children}
+    </div>
+  )
+}
